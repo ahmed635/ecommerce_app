e9efae0d2c676d0506796864b7fb62c91d5df8af
Ahmed Khaled <ahmed.khaled.aa123@gmail.com>
2024-10-03 17:39:22 +0300

    add lottie animations

--- lib/core/constants/app_image_assets.dart
+++ lib/core/constants/app_image_assets.dart
@@ -1,9 +1,14 @@
 class AppImageAssets {
   static const String rootImages = 'assets/images';
+  static const String rootLottie = 'assets/lottie';
 
   static const String logo = "$rootImages/logo.png";
   static const String onBoardingImageOne = "$rootImages/onboardingone.PNG";
   static const String onBoardingImageTwo = "$rootImages/onboardingtwo.PNG";
   static const String onBoardingImageThree = "$rootImages/onboardingthree.PNG";
   static const String onBoardingImageFour = "$rootImages/onboardingfour.PNG";
+  static const String loading = "$rootLottie/loading.json";
+  static const String nodata = "$rootLottie/nodata.json";
+  static const String offline = "$rootLottie/offline.json";
+  static const String server = "$rootLottie/server.json";
 }
